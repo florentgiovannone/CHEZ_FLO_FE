37cf922bbdb4d45be5ffbe1c058318bfdb55f041
Florent <giovannoneflorent@gmail.com>
2026-02-23 12:38:29 -0500

    Add Netlify build optimization config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://postgres-production-4f0c.up.railway.app/api';
+export const baseUrl = 'https://chezflobe-production.up.railway.app/api';
 // export const baseUrl = 'http://127.0.0.1:4000/api'
